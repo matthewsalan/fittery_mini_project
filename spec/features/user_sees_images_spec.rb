b9b5ed923fdbc491e62b82da26e93700cb3d5e6c
Alan Matthews <matthewsa1320@gmail.com>
2015-08-27 02:35:07 -0400

    First pass, green specs for user sees images spec

--- spec/features/user_sees_images_spec.rb
+++ spec/features/user_sees_images_spec.rb
@@ -4,6 +4,6 @@ feature "User sees images" do
   scenario "when they visit index page" do 
     visit root_path
 
-    expect(page).to have_css 
+    expect(page).to have_css("img[src*='image1.jpg']")
   end
 end
